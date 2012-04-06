42a1594c2a682ce348ba5e516ff871e53362d5bc
Mikah <mikakinha@hotmail.com>
2012-04-06 19:38:52 -0300

    Web Service ISMA

---
+++ server/moodle/local/isma/lang/en/local_isma.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Web service local plugin isma related strings
+ *
+ * @package   localwsisma
+ * @author    Mika Kuamoto - Paulo Silveira
+ */
+
+$string['pluginname'] = 'Web service Isma';
